2353d8b72ba4b86db28b93f49509111608e55702
neow24 <neow@metropolia.fi>
2025-03-10 12:50:41 +0200

    API V1

---
+++ backend/app.js
@@ -0,0 +1,28 @@
+require('dotenv').config()
+const express = require("express");
+const app = express();
+const jobRouter = require("./routes/jobRouter");
+const propertyRouter = require('./routes/propertyRouter');
+const { unknownEndpoint,errorHandler } = require("./middleware/customMiddleware");
+const connectDB = require("./config/db");
+const cors = require("cors");
+
+// Middlewares
+app.use(cors())
+app.use(express.json());
+
+connectDB();
+
+// Use the jobRouter for all "/jobs" routes
+app.use("/api/jobs", jobRouter);
+
+app.use('/api/properties', propertyRouter);
+
+app.use(unknownEndpoint);
+app.use(errorHandler);
+
+module.exports = app;
+
+// app.listen(process.env.PORT, () => {
+//   console.log(`Server running on port ${process.env.PORT}`)
+// })  
---
+++ backend/controllers/propertyControllers.js
@@ -0,0 +1,77 @@
+const Property = require('../models/propertyModel');
+const mongoose = require('mongoose');
+const getAllProperties=async(req,res)=>{
+    try{
+        const properties = await Property.find({}).sort({ createdAt: -1 });
+        res.status(200).json(properties);
+    }catch(error){
+        res.status(500).json({message:'Failed to retrieve properties'});
+    }
+};
+const createProperty=async(req,res)=>{
+    try{
+        const newProperty=await Property.create({...req.body});
+        res.status(201).json(newProperty);
+    }catch(error){
+        res.status(400).json({message:'Failed to create property',error:error.message});
+    }
+};
+const getPropertyById=async(req,res)=>{
+    const{propertyId}=req.params;
+    if(!mongoose.Types.ObjectId.isValid(propertyId)){
+        return res.status(400).json({message:'Invalid property ID'});
+    }
+    try{
+        const property=await Property.findById(propertyId);
+        if(property){
+            res.status(200).json(property);
+        }else{
+            res.status(404).json({message:'Property not found'});
+        }
+    }catch(error){
+        res.status(500).json({message:'Failed to retrieve property'});
+    }
+};
+const updateProperty=async(req,res)=>{
+    const{propertyId}=req.params;
+    if(!mongoose.Types.ObjectId.isValid(propertyId)){
+        return res.status(400).json({message:'Invalid property ID'});
+    }
+    try{
+        const updatedProperty=await Property.findOneAndUpdate(
+            {_id:propertyId},
+            {...req.body},
+            {new:true}
+        );
+        if(updatedProperty){
+            res.status(200).json(updatedProperty);
+        }else{
+            res.status(404).json({message:'Property not found'});
+        }
+    }catch(error){
+        res.status(500).json({message:'Failed to update property'});
+    }
+};
+const deleteProperty=async(req,res)=>{
+    const{propertyId}=req.params;
+    if(!mongoose.Types.ObjectId.isValid(propertyId)){
+        return res.status(400),json({message:'Invalid property ID'});
+    }
+    try{
+        const deletedProperty=await Property.findOneAndDelete({_id:propertyId});
+        if(deletedProperty){
+            res.status(204).send();
+        }else{
+            res.status(404).json({message:'Property not found'});
+        }
+    }catch(error){
+        res.status(500).json({message: 'Failed to delete property'});
+    }
+};
+module.exports={
+    getAllProperties,
+    getPropertyById,
+    createProperty,
+    updateProperty,
+    deleteProperty,
+};
